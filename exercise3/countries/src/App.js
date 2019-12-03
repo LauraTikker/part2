de2a0b655ded88b1ba3172095d1c509970d37732
Laura Tikker <tikker.laura@gmail.com>
2019-12-03 20:41:25 +0200

    files removed

--- exercise3/countries/src/App.js
+++ exercise3/countries/src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 const App = () => {
   const [countries, setCountries] = useState([])
   const [newSearch, setNewSearch] = useState("")
-  const [newCountry, setNewCountry] = useState("")
+  const [showCountry, setShowCountry] = useState("")
   const [newShow, setNewShow] = useState(false)
 
   const hook = () => {
@@ -27,6 +27,47 @@ const App = () => {
     event.preventDefault()
   }
 
+  const PrintCountries = (props) =>  {
+    if (newShow === false && showCountry === "")  {
+      return (
+      props.map(country => (
+      <li key={country.numericCode}>
+        {country.name}
+        <button onClick={handleCountry.bind(this, country)} type="Click">show</button></li>) 
+    ))
+    }
+
+    return (
+
+      <div>
+        <button onClick={handleBack} type="Click">back</button>
+        <h2>{showCountry.name}</h2>
+        <div>
+          <p>Capital {showCountry.capital}</p>
+          <p>Population {showCountry.population}</p>
+        </div>
+        <h2>Languages</h2>
+        <div>
+          <ul>
+            {PrintLanguages(showCountry.languages)}
+          </ul>
+        </div>
+        <div><img src={showCountry.flag} alt={'flag of ' + props[0].name} width={200}/></div>
+        <h2>Weather in {showCountry.capital}</h2>
+
+        <h5>temperature</h5>
+
+        <h5>wind</h5>
+
+      </div>
+    )
+            
+  }
+  const handleBack = () => {
+    newShow ? setNewShow(false) : setNewShow(true)
+      setShowCountry("")
+  }
+
   const CountriesList = () =>  {
     const countriesToShow = countries.filter(country => (country.name.toLowerCase()).includes(newSearch.toLowerCase()))
 
@@ -34,83 +75,54 @@ const App = () => {
       countriesToShow.length > 10 ? <div>Too many matches, specify another filter</div>
       : countriesToShow.length === 1 
       ? PrintOneCountry(countriesToShow)
-      : countriesToShow.map(country => (
-        <li key={country.numericCode}>
-        {country.name}
-        <button value={country.name} onClick={handleCountry} type="Click">show</button>
-        {newShow === true ?  
-          <div>
-          <h2>{country.name}</h2>
-          <div>
-            <p>capital {country.capital}</p>
-            <p>population {country.population}</p>
-          </div>
-          <h2>languages</h2>
-          <div>
-            <ul>
-              {PrintLanguages(country.languages)}
-            </ul>
-          </div>
-          <div><img src={country.flag}/></div>
-        </div>
-        {setNewShow(false)}
-        : <div></div>}
-        </li>
-
-      ))
+      : PrintCountries(countriesToShow) 
     )   
-
   }
 
-  const handleCountry = event => {
-      setNewShow(true);
-
+  const handleCountry = (props) => {
+      newShow ? setNewShow(false) : setNewShow(true)
+      setShowCountry(props)     
   }
 
   const PrintLanguages = (props) => {
-
     const languages = props.map(language => <li key={props.indexOf(language)} >{language.name}</li>)
 
     return (
       languages
     )
   }
-  const PrintOneCountry2 = (props) => {
-    
-    return (
-      <div>
-      <h2>{props.name}</h2>
-      <div>
-        <p>capital {props.capital}</p>
-        <p>population {props.population}</p>
-      </div>
-      <h2>languages</h2>
-      <div>
-        {/* <ul>
-          {PrintLanguages(props.languages)}
-        </ul> */}
-      </div>
-      <div><img src={props.flag}/></div>
-    </div>
-    )
-  }
 
   const PrintOneCountry = (props) => {
-    
+    const parameters = {
+      access_key: '7d5ac25c67b72e7e4ef6d0eed8de3a26',
+      query: props[0].name
+    }
+    const query = 'query = ' + props[0].name
+    const APiKey = 'access_key = 7d5ac25c67b72e7e4ef6d0eed8de3a26'
+    axios.get('http://api.weatherstack.com/current' ? {APiKey} : {query})
+    .then(response => {
+      console.log('promise fulfilled2')
+      console.log(response.data)
+    })
     return (
       <div>
       <h2>{props[0].name}</h2>
       <div>
-        <p>capital {props[0].capital}</p>
-        <p>population {props[0].population}</p>
+        <p>Capital {props[0].capital}</p>
+        <p>Population {props[0].population}</p>
       </div>
-      <h2>languages</h2>
+      <h2>Languages</h2>
       <div>
         <ul>
           {PrintLanguages(props[0].languages)}
         </ul>
       </div>
-      <div><img src={props[0].flag}/></div>
+      <div><img src={props[0].flag} alt={'flag of ' + props[0].name} width={200}/></div>
+      <h2>Weather in {props[0].capital}</h2>
+
+      <h5>temperature</h5>
+
+      <h5>wind</h5>
     </div>
     )
   }
